242b1cd4b165d9e5a0fe2482906465f1b7dcfb73
Benny <carlsson.benny93@gmail.com>
2018-10-12 21:54:35 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import vocabulary from "./vocabulary"
 import './App.css';
 
 class App extends Component {
+  state = {gameWords:[], correctAnswers:[],wrongAnswers:[], answerOptions:[], questionIndex:0}
+
+  componentDidMount = () => {
+    this.setState({gameWords:this.getWordsRandomized()})
+  }
+
+  getWordsRandomized = () => {
+    return this.shuffle(vocabulary.slice())
+  }
+
+  shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  nextQuestion = () => {
+
+  }
+
+  handleQuestion = () => {
+
+  }
+
   render() {
+    console.log(this.state.gameWords)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );

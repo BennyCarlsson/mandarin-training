47302b8a98664a580f850fcb9d07187dbb56d759
Benny <carlsson.benny93@gmail.com>
2019-05-31 15:11:41 +0800

    Updates

--- precache-manifest.f260bcb503a02bc732b7a66457926324.js
+++ precache-manifest.f260bcb503a02bc732b7a66457926324.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mandarin/static/js/runtime~main.9098bae0.js"
   },
   {
-    "revision": "3a3357027517343742b9",
-    "url": "/mandarin/static/js/main.3a335702.chunk.js"
+    "revision": "d5d1db3c169264391e42",
+    "url": "/mandarin/static/js/main.d5d1db3c.chunk.js"
   },
   {
     "revision": "0ede6e607bbd0011e4b1",
     "url": "/mandarin/static/js/1.0ede6e60.chunk.js"
   },
   {
-    "revision": "3a3357027517343742b9",
+    "revision": "d5d1db3c169264391e42",
     "url": "/mandarin/static/css/main.f25584e5.chunk.css"
   },
   {
-    "revision": "df2922445499002037f4b6a3e65d687e",
+    "revision": "d379e813772025b94a721a708b1a4d5b",
     "url": "/mandarin/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mandarin/precache-manifest.3023de46ed78e80ab931cf66f1cdf98b.js"
+  "/mandarin/precache-manifest.f260bcb503a02bc732b7a66457926324.js"
 );
 
 workbox.clientsClaim();

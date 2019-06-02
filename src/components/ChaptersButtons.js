13e2fcbf17a58fd710277bbaee54259e0137470d
Benny <carlsson.benny93@gmail.com>
2019-06-02 10:22:27 +0800

    added chapter 2.14

--- src/components/ChaptersButtons.js
+++ src/components/ChaptersButtons.js
@@ -189,6 +189,15 @@ const ChapterButtons = props => {
       >
         Book 2 Chapter 13
       </Button>
+      <Button
+        variant="contained"
+        size="large"
+        color="primary"
+        className={classes.button}
+        onClick={() => startChapter("chapter2.14")}
+      >
+        Book 2 Chapter 14
+      </Button>
     </Fragment>
   )
 }
--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -15,7 +15,7 @@ const PageFooter = () => {
         >
           <Icon className={"fab fa-github"} />
         </span>
-        <span className="footerVersionNr"> v.1.3.0</span>
+        <span className="footerVersionNr"> v.1.3.2</span>
       </span>
     </div>
   )

acadd72ac225e939525450ba3026144c38b21165
Benny <carlsson.benny93@gmail.com>
2019-06-09 15:22:50 +0800

    Updates

--- precache-manifest.8897e4793819a59740429d7f7826b787.js
+++ precache-manifest.8897e4793819a59740429d7f7826b787.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mandarin/static/js/runtime~main.9098bae0.js"
   },
   {
-    "revision": "5cbca8b910eb7aab1eda",
-    "url": "/mandarin/static/js/main.5cbca8b9.chunk.js"
+    "revision": "3048f97d89812cdf091a",
+    "url": "/mandarin/static/js/main.3048f97d.chunk.js"
   },
   {
     "revision": "0ede6e607bbd0011e4b1",
     "url": "/mandarin/static/js/1.0ede6e60.chunk.js"
   },
   {
-    "revision": "5cbca8b910eb7aab1eda",
+    "revision": "3048f97d89812cdf091a",
     "url": "/mandarin/static/css/main.f25584e5.chunk.css"
   },
   {
-    "revision": "bec51f46e22017160234dc71d0f1d372",
+    "revision": "bf4d697a51228d0d220a369a3ecd1181",
     "url": "/mandarin/index.html"
   }
 ];
